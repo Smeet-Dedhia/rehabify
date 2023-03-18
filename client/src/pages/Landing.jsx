14ff09e814c91a0647600406e901cbfcfcf5dc37
Prajna <prajnapprabhu3@gmail.com>
2023-03-18 13:21:40 +0530

    feat: hero section

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,9 +1,11 @@
+import HeroSection from "../components/HeroSection"
 import Navbar from "../components/Navbar"
 
 function Landing() {
   return (
     <div>
-      {/* <Navbar/> */}
+      <HeroSection/>
+    
     </div>
   )
 }
