2507e53cf7235705971bf4fdb79d6a44ea58dcc4
[Yaswanth-B] <[yaswanthonline@gmail.com]>
2023-03-18 13:52:20 +0530

    whatprovidebasic

--- client/src/components/Landing/WhatProvide.jsx
+++ client/src/components/Landing/WhatProvide.jsx
@@ -1,8 +1,20 @@
-
-
+import Whatprovidesub from "./Whatprovidesub"
+let featuresarray = ["Personalized communities",
+"Gamified progress tracking",
+"Professional support",
+"Counselling resources"]
 function WhatProvide() {
   return (
-    <div>WhatProvide</div>
+    <div>
+			<h1 className="text-3xl font-bold">What do we provide?</h1>
+			&nbsp;&nbsp;&nbsp;
+
+			<div className="flex flex-rows space-x-5">
+			
+					{featuresarray.map((text) => <Whatprovidesub text={text} />)}
+			
+			</div>
+		</div>
   )
 }
 
---
+++ client/src/components/Landing/Whatprovidesub.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Whatprovidesub = ({text}) => {
+	return (
+		<div className="rounded shadow-xl flex flex-col">
+			<h1>logo</h1>
+			<h1>{text}</h1>
+		</div>
+	)
+}
+
+export default Whatprovidesub
